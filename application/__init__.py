adc7446bca2d45a91df36b3c99342249edf78974
cathy.simms <cathy.simms@weareams.com>
2022-11-10 09:45:54 +0000

    models added

--- application/__init__.py
+++ application/__init__.py
@@ -3,9 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///survivorpool.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
 
+db = SQLAlchemy(app)
+
 from application import routes
+from application import models
 
 
 
