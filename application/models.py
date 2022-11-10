88da42994c4f51dab3d30a676a63e40e769ebdb8
cathy.simms <cathy.simms@weareams.com>
2022-11-10 12:40:25 +0000

    games database setup

--- application/models.py
+++ application/models.py
@@ -1,31 +1,44 @@
 from application import db
 
 class Teams(db.Model):
-    team_id = db.Column(db.Integer, primary_key=True)
+    __tablename__='teams'
+    id = db.Column(db.Integer, primary_key=True)
     team_name = db.Column(db.String(30), nullable=False)
     team_city = db.Column(db.String(30), nullable=False)
+    #picks = db.relationship('Picks', backref='teamspbr')
+   
 
 class Users(db.Model):
-    user_id = db.Column(db.Integer, primary_key=True)
+    __tablename__='users'
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), nullable=False)
     first_name = db.Column(db.String(30), nullable=False)
     surname = db.Column(db.String(30), nullable=False)
     email = db.Column(db.String(30), nullable=False, unique=True)
     lives = db.Column(db.Integer(), nullable=False, default = 3)
+    picks = db.relationship('Picks', backref='usersbr')
+
 
 class Games(db.Model):
-    game_id = db.Column(db.Integer, primary_key=True)
-    home_team = db.Column(db.Integer)
-    away_team = db.Column(db.Integer)
-    week_no = db.Column(db.Integer)
+    __tablename__='games'
+    id = db.Column(db.Integer, primary_key=True)
+    home_team = db.Column('home_team', db.Integer, db.ForeignKey('teams.id'), nullable=False)
+    away_team = db.Column('away_team', db.Integer, db.ForeignKey('teams.id'), nullable=False)
+    week_no = db.Column(db.Integer, nullable=False)
     home_score = db.Column(db.Integer, default = 0)
     away_score = db.Column(db.Integer, default = 0)
-    win_team = db.Column(db.Integer, default = 1)
+    complete = db.Column(db.Boolean, default=False)
+    home_win = db.Column(db.Boolean, default=False)
+    away_win = db.Column(db.Boolean, default=False)
+    picks = db.relationship('Picks', backref='gamesbr')
+    home = db.relationship('Teams', foreign_keys=[home_team])
+    away = db.relationship('Teams', foreign_keys=[away_team])
 
 class Picks(db.Model):
-    pick_id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer)
-    game_id = db.Column(db.Integer)
-    uteam_id = db.Column(db.Integer)
+    __tablename__='picks'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column('user_id',db.Integer, db.ForeignKey('users.id'), nullable=False)
+    game_id = db.Column('games_id',db.Integer, db.ForeignKey('games.id'), nullable=False)
+    #team_id = db.Column('team_id',db.Integer, db.ForeignKey('teams.id'),  nullable=False)
 
 
