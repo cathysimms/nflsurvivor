5af4517eb55b3463048331e485c48c27bf5e6536
cathy.simms <cathy.simms@weareams.com>
2022-11-11 11:48:51 +0000

    display all games for each week

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,6 @@
 from application import app
 from application.forms import HomeForm, UserForm
+from application.models import Games, Teams
 from flask import render_template, request, redirect, url_for
 
 @app.route("/", methods=['GET','POST'])
@@ -16,7 +17,17 @@ def home():
 def user(username):
     form = UserForm()
     week_no = "choose"
+    home_teams = []
+    away_teams = []
     if request.method =='POST':
         week_no = form.week.data
-    return render_template('user.html', name = username, form=form, week_no = week_no)
+        all_games = Games.query.filter_by(week_no = week_no).all()
+        for game in all_games:
+            home_num = Teams.query.filter_by(id = game.home_team).first()
+            away_num = Teams.query.filter_by(id = game.away_team).first()
+            home_teams.append(home_num.team_name)
+            away_teams.append(away_num.team_name)
+        return render_template('user.html', name = username, form=form, week_no = week_no, home_teams = home_teams, away_teams = away_teams)
+    else:
+        return render_template('user.html', name = username, form=form, week_no = week_no)
     
