9d406079660ce33aec64e2e71cdde3ef0f607bdc
cathy.simms <cathy.simms@weareams.com>
2022-11-15 10:30:50 +0000

    display league picks on home page

--- application/routes.py
+++ application/routes.py
@@ -6,13 +6,15 @@ from flask import render_template, request, redirect, url_for
 @app.route("/", methods=['GET','POST'])
 def home():
     form = HomeForm()
+    all_users = Users.query.all()
+    all_picks = db.session.query(Picks, Teams).join(Teams).all()
 
     if request.method =='POST':
         username = form.username.data
         users = Users.query.filter_by(username = username).all()
         for user in users:
             if username == user.username:
-                return redirect(url_for('user', username = user.username))  
+                return redirect(url_for('user', username = user.username, id = user.id))  
         new_user = Users(username = username)
         db.session.add(new_user)
         db.session.commit()
@@ -20,19 +22,19 @@ def home():
             new_pick = Picks(user_id = new_user.id, week_no = i, team_id = 33)
             db.session.add(new_pick)
         db.session.commit()
-        return redirect(url_for('user', username = new_user.username))
+        return redirect(url_for('user', username = new_user.username, id = new_user.id))
 
     else:
-        return render_template('home.html', form = form)
+        return render_template('home.html', form = form, all_users = all_users, all_picks = all_picks)
 
-@app.route("/<username>", methods =['GET','POST'])
-def user(username):
+@app.route("/<username>/<int:id>", methods =['GET','POST'])
+def user(username, id):
     form = UserForm() 
-    user = Users.query.filter_by(username = username).first()
+    # user = Users.query.filter_by(username = username).first()
     
     display_picks = ['no pick']
     for i in range(1,19):
-        pick = Picks.query.filter_by(user_id = user.id, week_no = i).first()
+        pick = Picks.query.filter_by(user_id = id, week_no = i).first()
         team = Teams.query.filter_by(id = pick.team_id).first()
         display_picks.append(team.team_name)
 
@@ -75,7 +77,7 @@ def update(week_no, user_id, team):
     for p in prev_pick:
         
         if p.team_id == teamid.id:
-            
+           
             return redirect(url_for('week', username = username, week_no = week_no))
    
     uppic = Picks.query.filter_by(week_no = week_no, user_id = user_id).first()
