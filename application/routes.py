314b3d1343d78dfb406ef81c15dc52f27d65de8a
cathy.simms <cathy.simms@weareams.com>
2022-11-15 14:02:03 +0000

    already chosen on games display

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,5 @@
 from application import app, db
-from application.forms import HomeForm, UserForm
+from application.forms import HomeForm
 from application.models import Games, Teams, Picks, Users
 from flask import render_template, request, redirect, url_for
 
@@ -10,18 +10,23 @@ def home():
     all_picks = db.session.query(Picks, Teams).join(Teams).all()
 
     if request.method =='POST':
+        
         username = form.username.data
         users = Users.query.filter_by(username = username).all()
+        
         for user in users:
             if username == user.username:
                 return redirect(url_for('user', username = user.username, id = user.id))  
+        
         new_user = Users(username = username)
         db.session.add(new_user)
         db.session.commit()
+        
         for i in range(1,19):
             new_pick = Picks(user_id = new_user.id, week_no = i, team_id = 33)
             db.session.add(new_pick)
         db.session.commit()
+        
         return redirect(url_for('user', username = new_user.username, id = new_user.id))
 
     else:
@@ -29,43 +34,37 @@ def home():
 
 @app.route("/<username>/<int:id>", methods =['GET','POST'])
 def user(username, id):
-    form = UserForm() 
+    #form = UserForm() 
     all_picks = db.session.query(Picks, Teams).join(Teams).all()
     user = Users.query.filter_by(id = id).first()
-    # display_picks = ['no pick']
-    # for i in range(1,19):
-    #     pick = Picks.query.filter_by(user_id = id, week_no = i).first()
-    #     team = Teams.query.filter_by(id = pick.team_id).first()
-    #     display_picks.append(team.team_name)
 
-    return render_template('user.html', name = username, form=form, all_picks = all_picks, user = user) 
+    return render_template('user.html', all_picks = all_picks, user = user) 
     
 @ app.route("/<username>-week<int:week_no>", methods=['GET', 'POST'])
 def week(username, week_no):
-    form = UserForm()
-    home_teams = []
+    
     user = Users.query.filter_by(username = username).first()
     all_picks = db.session.query(Picks, Teams).join(Teams).all()
+    all_user_picks = db.session.query(Picks, Teams).join(Teams).filter(Picks.user_id == user.id).all()
+    user_picks=[]
+    for pick, team in all_user_picks:
+        user_picks.append(team.team_name)
+
+    home_teams = []
     away_teams = []
-    game_id = []
-    num = 0
-    
-    # display_picks = ['no pick']
-    # for i in range(1,19):
-    #     pick = Picks.query.filter_by(user_id = user.id, week_no = i).first()
-    #     team = Teams.query.filter_by(id = pick.team_id).first()
-    #     display_picks.append(team.team_name)
 
     all_games = Games.query.filter_by(week_no = week_no).all()
+    num = Games.query.filter_by(week_no = week_no).count()
+
     for game in all_games:
         home_num = Teams.query.filter_by(id = game.home_team).first()
         away_num = Teams.query.filter_by(id = game.away_team).first()
+        
         home_teams.append(home_num.team_name)
         away_teams.append(away_num.team_name)
-        game_id.append(game)
+        
 
-        num += 1 
-    return render_template('week.html', form = form, name = username, num = num ,home_teams = home_teams, away_teams = away_teams, week_no = week_no, game_id = game_id, user_id = user.id, user = user, all_picks = all_picks)
+    return render_template('week.html', num = num ,home_teams = home_teams, away_teams = away_teams, week_no = week_no, user = user, all_picks = all_picks, user_picks = user_picks)
 
 @app.route('/update/<int:week_no>/<int:user_id>/<team>', methods=['GET','POST'])
 def update(week_no, user_id, team):
